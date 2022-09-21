9d5ff743026fd3e9783ff1ad08c67197c1bd0496
DESKTOP-T5EG0UE\user <chohmk>
2022-09-21 21:44:24 +0900

    lesson02 quiz

---
+++ src/main/java/com/quiz/lesson02/bo/StoreBO.java
@@ -0,0 +1,24 @@
+package com.quiz.lesson02.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quiz.lesson02.dao.StoreDAO;
+import com.quiz.lesson02.model.Store;
+
+@Service
+public class StoreBO {
+	
+	// input: 없음
+	// output: List<Store>
+
+	// DI(Dependency Injection): 의존성 주입
+	@Autowired
+	private StoreDAO storeDAO;
+	
+	public List<Store> getStoreList() {
+		return storeDAO.selectStoreList();
+	}
+}
